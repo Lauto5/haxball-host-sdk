b2218ce19cb489f4cd057f4359bf3af334fc7e05
LauLautaro1 <laugtaro6@gmail.com>
2026-03-08 10:05:23 -0300

    Haciendo los logs de Runtime.

--- src/bridge/runtime/browser/runtime.ts
+++ src/bridge/runtime/browser/runtime.ts
@@ -5,49 +5,54 @@ import { ILogger, ScopedLogger } from "../../../logger";
 import { RoomConfig } from "../../../types/haxball";
 
 export class Runtime implements IRuntime {
-    private logger: ILogger;
-    private browser: Browser;
-    private pages = new Map<string, HostPage>();
+  private logger: ILogger;
+  private browser: Browser;
+  private pages = new Map<string, HostPage>();
 
-    constructor(browser: Browser, rootLogger: ILogger) {
-        this.logger = new ScopedLogger(rootLogger, "Runtime");
-        this.logger.debug("🛠 Runtime creado con exito.");
-        this.browser = browser;
-    }
-    
+  constructor(browser: Browser, rootLogger: ILogger) {
+    this.logger = new ScopedLogger(rootLogger, "Runtime");
+    this.browser = browser;
+  }
+
   async launchPage(
-        rootLogger:ILogger,
-        pageId: string,
-        url: string,
-        config:RoomConfig,
-    ): Promise<void> {
-        if (this.pages.has(pageId)) {
-            throw new Error(`Page ${pageId} already exists`);
-        }
-      
+    rootLogger: ILogger,
+    pageId: string,
+    url: string,
+    config: RoomConfig,
+  ): Promise<void> {
+    try {
+      if (this.pages.has(pageId)) {
+        throw new Error(`Page ${pageId} already exists`);
+      }
+
       const page = await this.browser.newPage();
-      
+
       const hostPage: HostPage = new HostPage(rootLogger, pageId, page);
-    
+
       await hostPage.navigate(url);
-    
+
       await hostPage.injectEnvironmentBuilder();
-    
+
       await hostPage.launchHost(config);
-    
-      this.logger.debug("SE LANZO LA PAGINA");
+    } catch (error) {
+      if (error instanceof Error) {
+        this.logger.error("", { error: error.name, message: error.message });
+      }
     }
+  }
 
-    async closePage(pageId: string): Promise<void> {
-        const hostPage = this.pages.get(pageId);
-        if (!hostPage) {
-            throw new Error(`Page ${pageId} not found`);
-        }
-
-        this.logger.debug("🛠 cerrando pagina...", { pageId: pageId });
-        await hostPage.close();
-        this.pages.delete(pageId);
-
-        this.logger.debug("🛠 pagina cerrada con exito.", { pageId: pageId });
+  async closePage(pageId: string): Promise<void> {
+    try {
+      const hostPage = this.pages.get(pageId);
+      if (!hostPage) {
+        throw new Error(`Page ${pageId} not found`);
+      }
+      await hostPage.close();
+      this.pages.delete(pageId);
+    } catch (error) {
+      if (error instanceof Error) {
+        this.logger.error("", { error: error.name, message: error.message });
+      }
     }
+  }
 }
