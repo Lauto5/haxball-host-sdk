208de2a0d95ecade790ea47d788346ad09a59ccd
Lauto5 <Laugtaro6@gmail.com>
2026-03-19 02:53:12 -0300

    recordatorio, debo proseguir con la interface del box, definir correctamente.

--- src/bridge/box/box.interface.ts
+++ src/bridge/box/box.interface.ts
@@ -1,6 +1,7 @@
 import { RPCMessage } from "../rpc/rpcMessage";
 import { ITransport } from "../transport/transport.interface";
 
+// Definir una interface para las box's , RECORDATORIO DE LO QUE HAY QUE HACER.
 export interface IBox {
   injectTransport(transport: ITransport): void;
   connectToRPC(callback: (message:RPCMessage) => void): void;
