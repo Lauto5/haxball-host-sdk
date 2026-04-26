1687b5e25cbbff711a88c07d85b2b7d3179fab23
Lauto5 <Laugtaro6@gmail.com>
2026-04-25 22:09:21 -0300

    Documentando los interfaces de la observabilidad.

--- src/core/observability/logger/interfaces/logger.interface.ts
+++ src/core/observability/logger/interfaces/logger.interface.ts
@@ -1,3 +1,31 @@
+
+/**
+ * Interface for the logger.
+ * 
+ * **Implementation:** 
+ * 
+ * @example
+ * ```typescript
+ * const logger: ILogger = {
+ *   debug: (message: string, meta?: unknown) => {
+ *     console.log(message, meta)
+ *   },
+ *   info: (message: string, meta?: unknown) => {
+ *     console.log(message, meta)
+ *   },
+ *   warn: (message: string, meta?: unknown) => {
+ *     console.log(message, meta)
+ *   },
+ *   error: (message: string, meta?: unknown) => {
+ *     console.log(message, meta)
+ *   },
+ *   trace: (message: string, meta?: unknown) => {
+ *     console.log(message, meta)
+ *   },
+ * }
+ * ```
+ * 
+ */
 export interface ILogger {
   debug(message: string, meta?: unknown): void
   info(message: string, meta?: unknown): void
--- src/core/observability/metrics/interfaces/metrics.interface.ts
+++ src/core/observability/metrics/interfaces/metrics.interface.ts
@@ -1,4 +1,26 @@
 
+/**
+ * 
+ * metrics is used to keep a record of every action of the internal system of the sdk, it is used for statistics.
+ * 
+ * **example:**
+ * 
+ * @example
+ * ```typescript
+ * const metrics: IMetrics = {
+ *   increment: (name: string, value?: number, labels?: Record<string, string>) => {
+ *    // por ejemplo guardar en una base de datos o archivo.  
+ *   },
+ *   gauge: (name: string, value: number, labels?: Record<string, string>) => {
+ *    // por ejemplo guardar en una base de datos o archivo.  
+ *   },
+ *   observe: (name: string, value: number, labels?: Record<string, string>) => {
+ *    // por ejemplo guardar en una base de datos o archivo.  
+ *   },
+ * }
+ * ```
+ * 
+ */
 export interface IMetrics {
   
   increment(name: string, value?: number, labels?: Record<string, string>): void;
